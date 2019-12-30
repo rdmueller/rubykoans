1c7ea124dc18ece91350d4a1777a735ddfc9c859
Ralf D. Müller <ralf.d.mueller@gmail.com>
2019-12-30 12:13:19 +0000

    blocks

--- koans/about_blocks.rb
+++ koans/about_blocks.rb
@@ -68,16 +68,16 @@ class AboutBlocks < Neo::Koan
 
   def test_blocks_can_be_assigned_to_variables_and_called_explicitly
     add_one = lambda { |n| n + 1 }
-    assert_equal __, add_one.call(10)
+    assert_equal 11, add_one.call(10)
 
     # Alternative calling syntax
-    assert_equal __, add_one[10]
+    assert_equal 11, add_one[10]
   end
 
   def test_stand_alone_blocks_can_be_passed_to_methods_expecting_blocks
     make_upper = lambda { |n| n.upcase }
     result = method_with_block_arguments(&make_upper)
-    assert_equal __, result
+    assert_equal "JIM", result
   end
 
   # ------------------------------------------------------------------
@@ -87,10 +87,10 @@ class AboutBlocks < Neo::Koan
   end
 
   def test_methods_can_take_an_explicit_block_argument
-    assert_equal __, method_with_explicit_block { |n| n * 2 }
+    assert_equal 20, method_with_explicit_block { |n| n * 2 }
 
     add_one = lambda { |n| n + 1 }
-    assert_equal __, method_with_explicit_block(&add_one)
+    assert_equal 11, method_with_explicit_block(&add_one)
   end
 
 end
